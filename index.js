95d305fe65a81537c17d56e4a48198f637be5db6
Suneet25 <suneetpanigrahi2505@gmail.com>
2025-01-22 23:49:29 +0530

    SP | feat: Read about babel on day-2

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const htmlElem = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement(
-      "h1",
-      { id: "heading1", key: "child1heading1" },
-      "I am an H1 tag."
-    ),
-    React.createElement(
-      "h2",
-      { id: "heading2", key: "child1heading2" },
-      "I am an H2 tag."
-    ),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement(
-      "h1",
-      { id: "heading1", key: "child2heading1" },
-      "I am an H1 tag."
-    ),
-    React.createElement(
-      "h2",
-      { id: "heading2", key: "child2heading2" },
-      "I am an H2 tag."
-    ),
-  ]),
-]);
-console.log(htmlElem);
+// const htmlElem = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1", key: "child1" }, [
+//     React.createElement(
+//       "h1",
+//       { id: "heading1", key: "child1heading1" },
+//       "I am an H1 tag."
+//     ),
+//     React.createElement(
+//       "h2",
+//       { id: "heading2", key: "child1heading2" },
+//       "I am an H2 tag."
+//     ),
+//   ]),
+//   React.createElement("div", { id: "child2", key: "child2" }, [
+//     React.createElement(
+//       "h1",
+//       { id: "heading1", key: "child2heading1" },
+//       "I am an H1 tag."
+//     ),
+//     React.createElement(
+//       "h2",
+//       { id: "heading2", key: "child2heading2" },
+//       "I am an H2 tag."
+//     ),
+//   ]),
+// ]);
+const ReactComp = () => <div>This is a React Component!</div>;
+
+const title = (
+  <>
+    <div>Hello Suneet!</div>
+    {ReactComp()}
+    <ReactComp />
+    <ReactComp></ReactComp>
+  </>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(htmlElem);
+root.render(title);
