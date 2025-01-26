3cfd48799a79ad6f47837bf2ed12e6f0c9e1560a
Suneet25 <suneetpanigrahi2505@gmail.com>
2025-01-26 23:57:09 +0530

    SP | day-5 |feat: Integrated swiggy's api

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header";
+import Restaurants from "./src/components/Restaurants";
 
-// const htmlElem = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child1", key: "child1" }, [
-//     React.createElement(
-//       "h1",
-//       { id: "heading1", key: "child1heading1" },
-//       "I am an H1 tag."
-//     ),
-//     React.createElement(
-//       "h2",
-//       { id: "heading2", key: "child1heading2" },
-//       "I am an H2 tag."
-//     ),
-//   ]),
-//   React.createElement("div", { id: "child2", key: "child2" }, [
-//     React.createElement(
-//       "h1",
-//       { id: "heading1", key: "child2heading1" },
-//       "I am an H1 tag."
-//     ),
-//     React.createElement(
-//       "h2",
-//       { id: "heading2", key: "child2heading2" },
-//       "I am an H2 tag."
-//     ),
-//   ]),
-// ]);
-const ReactComp = () => <div>This is a React Component!</div>;
-
-const title = (
+const Container = (
   <>
-    <div>Hello Suneet!</div>
-    {ReactComp()}
-    <Header />
-    <ReactComp />
-    <ReactComp></ReactComp>
+    <Restaurants />
   </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(title);
+root.render(Container);
